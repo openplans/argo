72d2a802c35cf8333d116ed315e11a8081e3aa06
Aaron Ogle <atogle@gmail.com>
2012-08-08 08:39:21 -0400

    remove backone dependency

--- src/js/views.js
+++ src/js/views.js
@@ -2,164 +2,186 @@ var Argo = Argo || {};
 
 (function(A, $) {
   // A Legend for all of the layers
-  A.LegendView = Backbone.View.extend({
-    initialize: function(){
-      this.render();
-    },
-    events: {
-      'change .argo-legend-checkbox': 'toggleVisibility'
-    },
-    render: function(){
-      var $markup = $('<ul class="argo-legend-list"></ul>');
+  A.LegendView = function(o) {
+    var self = {
+      options: $.extend({}, o),
 
-      this.collection.each(function(model, i) {
-        $markup.append('<li class="argo-legend-item">' +
-          '<div class="argo-legend-desc">' +
-            '<div class="argo-legend-desc-title">'+model.get('title')+'</div>' +
-            '<div class="argo-legend-desc-content">'+model.get('description')+'</div>' +
-          '</div>' +
-          '<div class="argo-legend-title">' +
-            '<input id="argo-'+model.get('id')+'" data-layerid="'+model.get('id')+'" class="argo-legend-checkbox" type="checkbox"></input>' +
-            '<label for="argo-'+model.get('id')+'">'+model.get('title')+'</label>' +
-          '</div>' +
-        '</li>');
-      });
+      initialize: function(){
+        self.$el = $(self.options.el);
+        self.bindEvents();
+        self.render();
+      },
+      bindEvents: function() {
+        self.$el.on('change', '.argo-legend-checkbox', self.toggleVisibility);
+      },
+      render: function(){
+        var i, len = self.options.collection.length,
+            $markup = $('<ul class="argo-legend-list"></ul>');
 
-      this.$el.append($markup);
-    },
-    toggleVisibility: function(evt) {
-      var $cbox = $(evt.target),
-          id = $cbox.attr('data-layerid');
+        for (i=0; i<len; i++) {
+          model = self.options.collection[i];
 
-      if ($cbox.is(':checked')) {
-        this.collection.get(id).set('visible', true);
-      } else {
-        this.collection.get(id).set('visible', false);
+          $markup.append('<li class="argo-legend-item">' +
+            '<div class="argo-legend-desc">' +
+              '<div class="argo-legend-desc-title">'+model.title+'</div>' +
+              '<div class="argo-legend-desc-content">'+model.description+'</div>' +
+            '</div>' +
+            '<div class="argo-legend-title">' +
+              '<input id="argo-'+model.id+'" data-layer-index="'+i+'" class="argo-legend-checkbox" type="checkbox"></input>' +
+              '<label for="argo-'+model.id+'">'+model.title+'</label>' +
+            '</div>' +
+          '</li>');
+        }
+
+        self.$el.append($markup);
+      },
+      toggleVisibility: function(evt) {
+        var $cbox = $(evt.target),
+            i = $cbox.attr('data-layer-index');
+
+        if ($cbox.is(':checked')) {
+          $(self.options.collection[i]).trigger('show');
+        } else {
+          $(self.options.collection[i]).trigger('hide');
+        }
       }
-    }
-  });
+    };
+
+    self.initialize();
+    return self;
+  };
 
   // A view for stylable GeoJson layers
-  A.LayerView = Backbone.View.extend({
-    initialize: function(){
-      var self = this,
-          url = self.model.get('url'),
-          type = self.model.get('type') || 'jsonp',
-          getGeoJsonFunction = type === 'geoserver' ?
-            self.getGeoJsonFromGeoServer : self.getGeoJson;
+  A.LayerView = function(o) {
+    var self = {
+      options: $.extend({}, o),
 
-      getGeoJsonFunction(url, type, function(geoJson) {
-        if (geoJson) {
-          self.layer = L.geoJson(geoJson, {
-            pointToLayer: function (feature, latlng) {
-              return new L.CircleMarker(latlng);
-            },
-            onEachFeature: function(feature, layer) {
-              var style = self.getStyleRule(feature.properties),
-                  popupContent = self.getPopupContent(feature.properties);
+      initialize: function(){
+        var url = self.options.model.url,
+            type = self.options.model.type || 'jsonp',
+            getGeoJsonFunction = type === 'geoserver' ?
+              self.getGeoJsonFromGeoServer : self.getGeoJson;
 
-              // Only clickable if there is popup content; convert to bool
-              style.clickable = !!popupContent;
+        getGeoJsonFunction(url, type, function(geoJson) {
+          if (geoJson) {
+            self.layer = L.geoJson(geoJson, {
+              pointToLayer: function (feature, latlng) {
+                return new L.CircleMarker(latlng);
+              },
+              onEachFeature: function(feature, layer) {
+                var style = self.getStyleRule(feature.properties),
+                    popupContent = self.getPopupContent(feature.properties);
 
-              // Set the style manually since so I can use popupContent to set clickable
-              layer.setStyle(style);
+                // Only clickable if there is popup content; convert to bool
+                style.clickable = !!popupContent;
 
-              // Handle radius for circle marker
-              if (layer.setRadius && style.radius) {
-                layer.setRadius(style.radius);
+                // Set the style manually since so I can use popupContent to set clickable
+                layer.setStyle(style);
+
+                // Handle radius for circle marker
+                if (layer.setRadius && style.radius) {
+                  layer.setRadius(style.radius);
+                }
+
+                // Init the popup
+                if (popupContent) {
+                  layer.bindPopup(popupContent);
+                }
               }
+            });
+          } else {
+            console.error('GeoJSON could not be retrieved from: ', url);
+          }
+        });
 
-              // Init the popup
-              if (popupContent) {
-                layer.bindPopup(popupContent);
-              }
-            }
-          });
+        // Bind show and hide events
+        $(self.options.model).bind('show', self.show);
+        $(self.options.model).bind('hide', self.hide);
+      },
+      getGeoJsonFromGeoServer: function(url, type, callback) {
+        var callbackName = 'ArgoJsonpCallback_' + $.expando + '_' + $.now();
+        // Fetch the GeoJson from GeoServer
+        $.ajax({
+          url: url + '&format_options=callback:' + callbackName,
+          dataType: 'jsonp',
+          jsonpCallback: callbackName,
+          success: callback
+        });
+      },
+      getGeoJson: function(url, type, callback) {
+        // Fetch the GeoJson using the given type
+        $.ajax({
+          url: url,
+          dataType: type,
+          success: callback
+        });
+      },
+      // Get the style rule for this feature by evaluating the condition option
+      getStyleRule: function(properties) {
+        var rules = self.options.model.rules,
+            propertyRe = /\{\{property\}\}/g,
+            i, condition;
 
-          self.render();
-        } else {
-          console.error('GeoJSON could not be retrieved from: ', url);
+        for (i=0; i<rules.length; i++) {
+          // Replace the template with the property variable, not the value.
+          // this is so we don't have to worry about strings vs nums.
+          condition = rules[i].condition.replace(propertyRe, 'properties[self.options.model.property]');
+
+          // Simpler code plus a trusted source; negligible performance hit
+          if (eval(condition)) {
+            return rules[i].style;
+          }
         }
-      });
-
-      // Rerender on model change
-      self.model.bind('change', self.render, self);
-    },
-    getGeoJsonFromGeoServer: function(url, type, callback) {
-      var callbackName = 'ArgoJsonpCallback_' + $.expando + '_' + $.now();
-      // Fetch the GeoJson from GeoServer
-      $.ajax({
-        url: url + '&format_options=callback:' + callbackName,
-        dataType: 'jsonp',
-        jsonpCallback: callbackName,
-        success: callback
-      });
-    },
-    getGeoJson: function(url, type, callback) {
-      // Fetch the GeoJson using the given type
-      $.ajax({
-        url: url,
-        dataType: type,
-        success: callback
-      });
-    },
-    // Get the style rule for this feature by evaluating the condition option
-    getStyleRule: function(properties) {
-      var self = this,
-          rules = self.model.get('rules'),
-          propertyRe = /\{\{property\}\}/g,
-          i, condition;
-
-      for (i=0; i<rules.length; i++) {
-        // Replace the template with the property variable, not the value.
-        // this is so we don't have to worry about strings vs nums.
-        condition = rules[i].condition.replace(propertyRe, 'properties[self.model.get("property")]');
-
-        // Simpler code plus a trusted source; negligible performance hit
-        if (eval(condition)) {
-          return rules[i].style;
-        }
+        return null;
+      },
+      // Get the popup content and replace the variable
+      getPopupContent: function(properties) {
+        return (self.options.model.popupContent || '').replace('{{value}}', properties[self.options.model.property]);
+      },
+      show: function(){
+        self.options.map.closePopup();
+        self.options.map.addLayer(self.layer);
+      },
+      hide: function() {
+        self.options.map.closePopup();
+        self.options.map.removeLayer(self.layer);
       }
-      return null;
-    },
-    // Get the popup content and replace the variable
-    getPopupContent: function(properties) {
-      return (this.model.get('popupContent') || '').replace('{{value}}', properties[this.model.get('property')]);
-    },
-    render: function(){
-      // Adds or removes the layer based on visibility
-      this.options.map.closePopup();
-      if (this.model.get('visible') && !this.options.map.hasLayer(this.layer)) {
-        this.options.map.addLayer(this.layer);
-      }
-      if (!this.model.get('visible') && this.options.map.hasLayer(this.layer)) {
-        this.options.map.removeLayer(this.layer);
-      }
-    }
-  });
+    };
+
+    self.initialize();
+    return self;
+  };
 
   // A view for maps with a legend and stylable GeoJson layers
-  A.MapView = Backbone.View.extend({
-    initialize: function() {
-      var self = this,
-          i, layerModel,
-          baseTileUrl = 'http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png',
-          baseTileAttribution = 'Map data &copy; OpenStreetMap contributors, CC-BY-SA <a href="http://mapbox.com/about/maps" target="_blank">Mapbox Terms &amp; Feedback</a>',
-          baseTile = new L.TileLayer(baseTileUrl, {attribution: baseTileAttribution});
+  A.MapView = function(o) {
+    var self = {
+      options: $.extend({}, o),
 
-      // Init the map
-      self.map = new L.Map(self.el, self.options.map);
-      self.map.addLayer(baseTile);
+      initialize: function() {
+        var i, len = self.options.collection.length,
+            layerModel,
+            baseTileUrl = 'http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png',
+            baseTileAttribution = 'Map data &copy; OpenStreetMap contributors, CC-BY-SA <a href="http://mapbox.com/about/maps" target="_blank">Mapbox Terms &amp; Feedback</a>',
+            baseTile = new L.TileLayer(baseTileUrl, {attribution: baseTileAttribution});
 
-      // Cache the layers views
-      self.layers = {};
+        // Init the map
+        self.map = new L.Map($(self.options.el)[0], self.options.map);
+        self.map.addLayer(baseTile);
 
-      this.collection.each(function(model, i) {
-        self.layers[model.get('id')] = new A.LayerView({
-          map: self.map,
-          model: model
-        });
-      });
-    }
-  });
+        // Cache the layers views
+        self.layers = {};
+
+        for (i=0; i<len; i++) {
+          model = self.options.collection[i];
+          self.layers[model.id] = new A.LayerView({
+            map: self.map,
+            model: model
+          });
+        }
+      }
+    };
+
+    self.initialize();
+    return self;
+  };
 })(Argo, jQuery);
